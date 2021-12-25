b2557cd681ab33e23a1b133581f07df401fe4481
codexairy <2689225702@qq.com>
2021-12-25 20:16:42 +0800

    forth work

---
+++ src/forth/TestCircleWithPrivateDataFields.java
@@ -0,0 +1,13 @@
+package forth;
+
+public class TestCircleWithPrivateDataFields {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Circle3 myCircle = new Circle3(5.0);
+		System.out.println("The area of the circle of radius " + myCircle.getRadius() + " is " + myCircle.getArea());
+		myCircle.setRadius(myCircle.getRadius() * 1.1);
+		System.out.println("The area of the circle of radius " + myCircle.getRadius() + " is " + myCircle.getArea());
+		System.out.println("The number of objects created is " + Circle3.getNumberOfObjects());
+	}
+}
